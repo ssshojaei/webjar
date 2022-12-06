1297b7a102ef4af11e78428fd8436765a349c7e6
Saleh Shojaei <ordinarysaleh@pm.me>
2022-12-06 03:34:53 +0330

    chore: add breadcrumbs links

--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -1,8 +1,24 @@
+import Breadcrumbs from 'components/Breadcrumbs'
 import Layout from 'components/Layout'
 import React from 'react'
 
+const links = [
+  {
+    link: '/pages',
+    label: 'صفحه‌ها',
+  },
+  {
+    label: 'خدمات',
+  },
+]
+
 const services = () => {
-  return <Layout>services</Layout>
+  return (
+    <Layout>
+      <Breadcrumbs pages={links} />
+      <h1>services</h1>
+    </Layout>
+  )
 }
 
 export default services
